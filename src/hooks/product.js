b9d221113a8e28c3f36ef5c08be135072516a07c
Erik Christensen <erik.christensen@bigcommerce.com>
2022-03-22 14:19:15 -0700

    feat(storefront) Add hook for quantity changed

--- src/hooks/product.js
+++ src/hooks/product.js
@@ -9,11 +9,21 @@ export default class extends BaseHooks {
         super();
 
         this.optionsChange();
+        this.quantityChange();
     }
 
     optionsChange() {
         this.subscribe('change', '[data-product-option-change]', (event, target) => {
             this.emit('product-option-change', event, target);
+        });
+    }
+
+    quantityChange() {
+        this.subscribe('change', '[data-quantity-change]', (event, target) => {
+            this.emit('product-quantity-change', event, target);
+        });
+        this.subscribe('click', '[data-quantity-change] > button', (event, target) => {
+            this.emit('product-quantity-change', event, target);
         });
     }
 }
